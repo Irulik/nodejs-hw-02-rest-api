82624898f35bf2617cbca5bbf09c9f355bea66ab
Irulik <123405063+Irulik@users.noreply.github.com>
2023-11-02 09:55:22 +0100

    added checkPhone

--- models/contact.js
+++ models/contact.js
@@ -21,10 +21,20 @@ const contactSchema = new Schema({
 
 contactSchema.post("save", handleMongooseError);
 
+function checkPhone(value) {
+  return /^(\(\d{3}\)\s\d{3}-\d{4})$/.test(value);
+}
+
 const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
-  phone: Joi.string().pattern(new RegExp('^[0-9]{10}$')).required(),
+  // phone: Joi.string().pattern(new RegExp('^[0-9]{10}$')).required(),
+  phone: Joi.string().custom((value, helper) => {
+    if (!checkPhone(value)) {
+      return helpers.error("phone.invalidFormat", { message: "Please enter a phone number in the format (XXX) XXX-XXXX" });
+    }
+    return value;
+  }),
   favorite: Joi.boolean(),
 });
 
