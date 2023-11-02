3476e5a62ec1403f36cd71b00b0dc7c828da0394
Irulik <123405063+Irulik@users.noreply.github.com>
2023-11-02 19:08:21 +0100

    added registration

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 require("dotenv").config();
 
 
+const authRouter = require("./routes/api/auth");
 const contactsRouter = require('./routes/api/contacts')
 const { default: mongoose } = require('mongoose')
 
@@ -17,6 +18,8 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+
+app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
