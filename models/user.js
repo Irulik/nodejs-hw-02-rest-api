9362a4427987e9d0829d9d980b17817f376eb3fc
Irulik <123405063+Irulik@users.noreply.github.com>
2023-11-14 10:32:45 +0100

    done hw06

--- models/user.js
+++ models/user.js
@@ -24,6 +24,14 @@ const userSchema = new Schema({
   avatarURL: {
     type: String,
     required: true,
+  },
+  verify: {
+    type: Boolean, 
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
   }
 }, { versionKey: false, timestamps: true });
 
@@ -33,6 +41,10 @@ const registerSchema = Joi.object({
     // name: Joi.string().required(),
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().min(6).required(),
+})
+
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
 })
 
 const loginSchema = Joi.object({
@@ -46,6 +58,7 @@ const subscriptionSchema = Joi.object({
 
 const schemas = {
     registerSchema,
+    emailSchema,
     loginSchema,
     subscriptionSchema,
 }
